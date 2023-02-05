7f2302c689a9ddee1eac84c1204f09b744f3c560
ved-s <vedomir.samarin.03@gmail.com>
2023-02-05 15:22:23 +1100

    add crash details window

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿
-using var game = new Cornifer.Main();
-game.Run();
+#if !DEBUG
+using System;
+using System.Windows.Forms;
+
+try
+{
+#endif
+
+    using var game = new Cornifer.Main();
+    game.Run();
+
+#if !DEBUG
+}
+catch (Exception ex)
+{
+    MessageBox.Show($"Send this error when asking for help\n\n{ex.GetType().Name}: {ex.Message}\n{ex.StackTrace}", "Cornifer has crashed!");
+}
+#endif
